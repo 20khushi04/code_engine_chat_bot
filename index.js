2d7424e2554fc85277e4d58378de4618ae1c7fb3
20khushi04 <104572317+20khushi04@users.noreply.github.com>
2024-06-26 10:02:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+
+var PORT;
+var Cloudant = require('@cloudant/cloudant');
+
+
+if (process.env.PORT) {
+  PORT = process.env.PORT;
+} else {
+  PORT = 8000;
+}
+var Cloudant = require('@cloudant/cloudant');
+var url ="https://apikey-v2-sngfqsvcquzuejrr5lfojyp02z6yysrqfmtw8qx5ltw:cc4dd93ad3558e4628cf1af621c0fd84@4e3eea21-abc4-49b0-a207-0d540bdd5773-bluemix.cloudantnosqldb.appdomain.cloud";
+var username = "apikey-v2-sngfqsvcquzuejrr5lfojyp02z6yysrqfmtw8qx5ltw";
+var password = "cc4dd93ad3558e4628cf1af621c0fd84";
+var app = express();
+const bodyParser = require('body-parser');
+//const cors = require('cors');
+//app.use(cors());
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+////////////
+app.post('/insert', (req, res) => {
+
+  const { name, email, database } = req.body;
+  
+  const data = {
+      name: name,
+      email: email
+  };
+///////
+Cloudant({ url: url, username: username, password: password }, function(err, cloudant, pong) {
+  if (err) {
+    return console.log('Failed to initialize Cloudant: ' + err.message);
+  }
+console.log(pong); // {"couchdb":"Welcome","version": ..
+
+cloudant.use(database).insert({ "name": name, "email": email } , (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(data); // { ok: true, id: 'rabbit', ...
+      }
+    });
+});
+
+});
+
+
+app.listen(PORT);
+//console.log(message.getPortMessage() + PORT);
+
+
+
